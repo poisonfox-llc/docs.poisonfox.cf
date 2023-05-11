ef57170e35ebb5a5b53495f591d308adc6437a6f
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2023-05-11 15:04:41 +0200

    Delete gsharp directory

--- gsharp/docs.poisonfox.cf-main/javascript/copy-code.js
+++
@@ -1,8 +0,0 @@
-const copyBtn = document.querySelectorAll("#copy-btn");
-
-copyBtn.forEach((btn, index) => btn.addEventListener("click", () => {
-  const text = codePre[index].textContent;
-  navigator.clipboard.writeText(text)
-    .then(() => console.log('Text copied to clipboard'))
-    .catch((err) => console.error('Failed to copy text: ', err));
-}));
