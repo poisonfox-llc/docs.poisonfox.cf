682a3e87c1ba131d6487309bc14c6769c7668fb0
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2023-07-30 21:21:43 +0200

    Update theme.js

--- gsharp/javascript/theme.js
+++ gsharp/javascript/theme.js
@@ -3,7 +3,7 @@ const leftSideNav = document.querySelector(".left-side-nav");
 const header = document.querySelector("header");
 const themeBtn = document.querySelector(".theme-btn");
 
-let darkThemeCookie = document.cookie.split(";").find(cookie => cookie.trim().startsWith("darkTheme=")).split("=")[1];
+let darkThemeCookie = (document.cookie.split(";").find(cookie => cookie.trim().startsWith("darkTheme=")).split("=")[1] === "true");
 									  
 themeBtn.addEventListener("click", () => {
 	body.classList.toggle("dark");
