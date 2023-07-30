b25f0c02d8eb626d63565ccf2ed589a5fb663934
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2023-07-30 20:38:06 +0200

    Update theme.js

--- gsharp/javascript/theme.js
+++ gsharp/javascript/theme.js
@@ -8,11 +8,11 @@ themeBtn.addEventListener("click", () => {
 	leftSideNav.classList.toggle("dark");
 	header.classList.toggle("dark");
 	
-    const isDarkTheme = body.classList.contains("dark");
-    
-    const expirationDate = new Date();
-    expirationDate.setTime(expirationDate.getTime() + (30 * 24 * 60 * 60 * 1000));
-    document.cookie = `darkTheme=${isDarkTheme};expires=${expirationDate.toUTCString()};path=/`;
+	const isDarkTheme = body.classList.contains("dark");
+	
+	const expirationDate = new Date();
+	expirationDate.setTime(expirationDate.getTime() + (30 * 24 * 60 * 60 * 1000));
+	document.cookie = `darkTheme=${isDarkTheme};expires=${expirationDate.toUTCString()};path=/`;
 });
 
 themeBtn.addEventListener("touch", () => {
@@ -20,11 +20,11 @@ themeBtn.addEventListener("touch", () => {
 	leftSideNav.classList.toggle("dark");
 	header.classList.toggle("dark");
 	
-    const isDarkTheme = body.classList.contains("dark");
-    
-    const expirationDate = new Date();
-    expirationDate.setTime(expirationDate.getTime() + (30 * 24 * 60 * 60 * 1000));
-    document.cookie = `darkTheme=${isDarkTheme};expires=${expirationDate.toUTCString()};path=/`;
+	const isDarkTheme = body.classList.contains("dark");
+	
+	const expirationDate = new Date();
+	expirationDate.setTime(expirationDate.getTime() + (30 * 24 * 60 * 60 * 1000));
+	document.cookie = `darkTheme=${isDarkTheme};expires=${expirationDate.toUTCString()};path=/`;
 });
 
 window.addEventListener("keydown", (e) => {
@@ -43,11 +43,13 @@ window.addEventListener("keydown", (e) => {
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", (e) => {
 	const isDarkTheme = e.matches;
-		
-	const expirationDate = new Date();
-	expirationDate.setTime(expirationDate.getTime() + (30 * 24 * 60 * 60 * 1000));
-	document.cookie = `darkTheme=${isDarkTheme};expires=${expirationDate.toUTCString()};path=/`;
-	if (isDarkTheme) {
+	const darkThemeCookie = document.cookie.split(";").find(cookie => cookie.trim().startsWith("darkTheme="));
+
+	if (isDarkTheme && typeof(darkThemeCookie) != "boolean") {
+		body.classList.add("dark");
+		leftSideNav.classList.add("dark");
+		header.classList.add("dark");
+	} else if (darkThemeCookie) {
 		body.classList.add("dark");
 		leftSideNav.classList.add("dark");
 		header.classList.add("dark");
@@ -61,11 +63,13 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", (e)
 window.addEventListener("load", (e) => {
 	const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 	const isDarkTheme = mediaQuery.matches;
-		
-	const expirationDate = new Date();
-	expirationDate.setTime(expirationDate.getTime() + (30 * 24 * 60 * 60 * 1000));
-	document.cookie = `darkTheme=${isDarkTheme};expires=${expirationDate.toUTCString()};path=/`;
-	if (isDarkTheme) {
+	const darkThemeCookie = document.cookie.split(";").find(cookie => cookie.trim().startsWith("darkTheme="));
+
+	if (isDarkTheme && typeof(darkThemeCookie) != "boolean") {
+		body.classList.add("dark");
+		leftSideNav.classList.add("dark");
+		header.classList.add("dark");
+	} else if (darkThemeCookie) {
 		body.classList.add("dark");
 		leftSideNav.classList.add("dark");
 		header.classList.add("dark");
@@ -79,15 +83,15 @@ window.addEventListener("load", (e) => {
 const darkThemeCookie = document.cookie.split(";").find(cookie => cookie.trim().startsWith("darkTheme="));
 
 if (darkThemeCookie) {
-    const isDarkTheme = darkThemeCookie.split("=")[1] === "true";
-    
-    if (isDarkTheme) {
-        body.classList.add("dark");
-        leftSideNav.classList.add("dark");
-        header.classList.add("dark");
-    } else {
-        body.classList.remove("dark");
-        leftSideNav.classList.remove("dark");
-        header.classList.remove("dark");
-    }
+	const isDarkTheme = darkThemeCookie.split("=")[1] === "true";
+	
+	if (isDarkTheme) {
+		body.classList.add("dark");
+		leftSideNav.classList.add("dark");
+		header.classList.add("dark");
+	} else {
+		body.classList.remove("dark");
+		leftSideNav.classList.remove("dark");
+		header.classList.remove("dark");
+	}
 }
